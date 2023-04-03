c8069b4447326ef016f1e70df2ef5730b93b40e4
AlessioRoman <alessio.romano02dev@gmail.com>
2023-04-03 16:16:24 +0200

    added resources and color palet to tailwind.cjs

--- jsnack-interactive/src/App.jsx
+++ jsnack-interactive/src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-red-500">Hi</h1>
+    </div>
+  );
 }
 
 export default App;
--- jsnack-interactive/tailwind.config.cjs
+++ jsnack-interactive/tailwind.config.cjs
@@ -2,6 +2,12 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      purpleBg: "#0b111e",
+      purpleBgAlt: "#151c28",
+      purpleText: "#9584e6",
+      greyAccent: "#a0a5ab",
+    },
     extend: {},
   },
   plugins: [],
