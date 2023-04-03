14d264ccc1cfb1592bd314eb1f01418e24d369e7
AlessioRoman <alessio.romano02dev@gmail.com>
2023-04-03 17:05:27 +0200

    added icon and data for projects

---
+++ jsnack-interactive/src/data/projects.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    name: "JSnack 1",
+    description:
+      "L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.",
+  },
+  {
+    name: "JSnack 2",
+    description:
+      "L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.",
+  },
+  {
+    name: "JSnack 3",
+    description:
+      "Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti",
+  },
+  {
+    name: "JSnack 4",
+    description:
+      "In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.",
+  },
+  {
+    name: "JSnack 5",
+    description:
+      "Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array. Stampa l'array alla fine.",
+  },
+  {
+    name: "JSnack 6",
+    description:
+      "Fate generare un numero random da 0 a 10 al computer, e chiedete all'utente di inserire un suo numero. Se il numero scelto dall'utente è uguale al numero del computer informate l'utente che ha vinto, altrimenti ha perso.",
+  },
+  {
+    name: "JSnack 7",
+    description:
+      "Scrivere un programma che stampi la tabellina del 2, fino al numero 1000. Modificate poi il programma in modo che venga chiesto all'utente il numero massimo consentito, e stampare tutta la tabellina del 2 fino al numero inserito",
+  },
+];
