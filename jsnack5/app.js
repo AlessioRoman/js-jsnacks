a2ccad66a80b49e3ecba83ebe2e0a7af277049ab
AlessioRoman <alessio.romano02dev@gmail.com>
2023-04-03 15:02:18 +0200

    completed index.html app.js

--- jsnack5/app.js
+++ jsnack5/app.js
@@ -1,1 +1,13 @@
-const hi = prompt()
+let numbers = [];
+
+for (let i = 0; i < 6; i++) {
+  let tmp = parseInt(prompt("Inserisci un numero"));
+
+  if (tmp % 2 != 0) {
+    numbers.push(tmp);
+  }
+}
+
+for (let i = 0; i < numbers.length; i++) {
+  document.getElementById("result").innerHTML += toString.numbers[i];
+}
