3ffd868cb64bfb92f096de9e41cf1ae1f53a3bbb
AlessioRoman <alessio.romano02dev@gmail.com>
2023-04-03 14:39:03 +0200

    completed index.html and app.js

--- jsnack4/app.js
+++ jsnack4/app.js
@@ -1,1 +1,27 @@
-const hi = prompt()
+const invitati = [
+  "Claudia",
+  "Mary",
+  "Sara",
+  "Valerio",
+  "Marika",
+  "Bryan",
+  "Emanuele",
+  "Vincenzo",
+];
+const researchKey = prompt("Inserisci il tuo nome con la lettera maiuscola");
+
+const checkInvites = () => {
+  for (let i = 0; i <= invitati.length; i++) {
+    if (invitati[i] === researchKey) {
+      return true;
+    }
+  }
+  return false;
+};
+
+if (checkInvites()) {
+  document.getElementById("result").innerHTML =
+    "Sei invitato alla festa del grande Gatsby";
+} else {
+  document.getElementById("result").innerHTML = "Non sei stato invitato";
+}
