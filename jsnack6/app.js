6d3a341a572bf6717e47e3b86e7b03203ec4cd81
AlessioRoman <alessio.romano02dev@gmail.com>
2023-04-03 15:34:30 +0200

    completed index.html and app.js

--- jsnack6/app.js
+++ jsnack6/app.js
@@ -1,1 +1,8 @@
-const hi = prompt()
+const randomNumber = Math.floor(Math.random() * 11);
+const userNumber = parseInt(prompt("Inserisci un numero tra 0 e 10"));
+
+if (randomNumber == userNumber) {
+  document.getElementById("result").innerHTML = "Complimenti hai vinto";
+} else {
+  document.getElementById("result").innerHTML = "Mi dispiace hai perso";
+}
--- jsnack7/app.js
+++ jsnack7/app.js
@@ -1,1 +1,9 @@
-const hi = prompt()
+const maxNumber = parseInt(prompt("Fino a che numero devo stampare?"));
+let counter = 0;
+
+do {
+  counter = counter + 2;
+  document.getElementById("result").innerHTML += `
+   <li class="list-group-item"> ${counter} </li>
+  `;
+} while (counter < maxNumber && counter < 1000);
