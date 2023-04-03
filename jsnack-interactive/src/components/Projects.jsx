677a49b712861c82bba0a760d1858a61cf5e8cd1
AlessioRoman <alessio.romano02dev@gmail.com>
2023-04-03 19:09:55 +0200

    implemented grid instead of flex

--- jsnack-interactive/src/components/Projects.jsx
+++ jsnack-interactive/src/components/Projects.jsx
@@ -3,41 +3,109 @@ function jsnack1() {
   const secondNumber = parseInt(prompt("Inserisci il secondo numero: "));
 
   if (firstNumber > secondNumber) {
-    document.getElementById("result").innerHTML = firstNumber;
+    alert(firstNumber);
   } else if (firstNumber < secondNumber) {
-    document.getElementById("result").innerHTML = secondNumber;
+    alert(secondNumber);
   } else {
-    document.getElementById("result").innerHTML = "I due numeri sono uguali";
+    alert("I due numeri sono uguali");
   }
 }
 
 function jsnack2() {
-  console.log(hi);
+  const firstWord = prompt("Inserisci la prima parola: ");
+  const secondWord = prompt("Inserisci la seconda parola: ");
+
+  if (firstWord.length > secondWord.length) {
+    alert(secondWord + " " + firstWord);
+  } else if (firstWord.length < secondWord.length) {
+    alert(firstWord + " " + secondWord);
+  } else {
+    alert("Le due parole sono uguali!");
+  }
 }
 
 function jsnack3() {
-  console.log(hi);
+  const numbers = [];
+  let sum = 0;
+
+  for (let i = 0; i < 10; i++) {
+    numbers.push(parseInt(prompt("Inserisci un numero")));
+    sum += numbers[i];
+  }
+
+  alert(sum);
 }
 
 function jsnack4() {
-  console.log(hi);
+  const invitati = [
+    "Claudia",
+    "Mary",
+    "Sara",
+    "Valerio",
+    "Marika",
+    "Bryan",
+    "Emanuele",
+    "Vincenzo",
+  ];
+  const researchKey = prompt("Inserisci il tuo nome con la lettera maiuscola");
+
+  const checkInvites = () => {
+    for (let i = 0; i <= invitati.length; i++) {
+      if (invitati[i] === researchKey) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  if (checkInvites()) {
+    alert("Sei invitato alla festa del grande Gatsby");
+  } else {
+    alert("Non sei stato invitato");
+  }
 }
 
 function jsnack5() {
-  console.log(hi);
+  let numbers = [];
+
+  for (let i = 0; i < 6; i++) {
+    let tmp = parseInt(prompt("Inserisci un numero"));
+
+    if (tmp % 2 != 0) {
+      numbers.push(tmp);
+    }
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    alert(numbers[i]);
+  }
 }
 
 function jsnack6() {
-  console.log(hi);
+  const randomNumber = Math.floor(Math.random() * 11);
+  const userNumber = parseInt(prompt("Inserisci un numero tra 0 e 10"));
+
+  if (randomNumber == userNumber) {
+    alert("Complimenti hai vinto");
+  } else {
+    alert("Mi dispiace hai perso");
+  }
 }
 
 function jsnack7() {
-  console.log(hi);
+  const maxNumber = parseInt(prompt("Fino a che numero devo stampare?"));
+  let counter = 0;
+
+  do {
+    counter = counter + 2;
+    console.log(counter);
+  } while (counter < maxNumber && counter < 1000);
+  alert("Apri la console!");
 }
 
 function Projects({ title, identifier, description }) {
   return (
-    <div className=" text-purpleText bg-purpleBgAlt w-1/5 rounded-lg px-6 py-4 flex flex-col items-start justify-between shadow-lg border border-grey border-opacity-80">
+    <div className=" text-purpleText bg-purpleBgAlt rounded-lg px-6 py-4 flex flex-wrap flex-col items-start justify-between shadow-lg border border-grey border-opacity-80">
       <div>
         <h1 className="text-white text-2xl font-semibold mb-4">{title}</h1>
         <p className="text-white">{description}</p>
